76b6fae92970e893e10d7d45efe7d70c0ce05e33
rayGreycloud <raygreycloud@gmail.com>
2023-04-13 11:20:22 -0700

    useLayoutEffect to dynamically set title

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,26 @@
+import { useLayoutEffect } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 
 import MealItem from '../components/MealItem';
 
-import { MEALS } from '../data/dummy-data';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
   const { categoryId } = route.params;
 
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0
   );
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle
+    });
+  }, [navigation, categoryId]);
 
   const renderMealItem = (itemData) => {
     const item = itemData.item;
